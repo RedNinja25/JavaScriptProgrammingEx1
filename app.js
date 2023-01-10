5b71a852fba09cf7b5d6131cb7ec5bb10a433a47
Davis Cordeiro <cordeirodavis18@gmail.com>
2023-01-10 12:21:58 -0400

    Working on ex1

---
+++ app.js
@@ -0,0 +1,18 @@
+// Author: Davis Cordeiro
+// Title: JavaScript Ex1 working with Variables
+// Date: 01/10/2023
+
+let myFirstIntegerVariable = 4;
+let myFirstFloatVariable = 9.9;
+let myFirstBooleanVariable = true;
+let myFirstName = 'Davis';
+let myLastName = 'Cordeiro';
+let myNullVariable = null; //intentional abscence of value.
+let myUndefinedVariable; // not assigned value, undefined.
+
+console.log(myFirstIntegerVariable);
+console.log(myFirstFloatVariable);
+console.log(myFirstBooleanVariable);
+console.log(myFirstName + ' ' + myLastName);
+console.log(myNullVariable);
+console.log(myUndefinedVariable);
