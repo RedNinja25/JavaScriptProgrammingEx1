16d2d7ed708edf967bdc8584a0747da7e14c52ee
Davis Cordeiro <cordeirodavis18@gmail.com>
2023-01-17 10:34:27 -0400

    Commit Pratice 1

---
+++ pratice.js
@@ -0,0 +1,10 @@
+let computerModel = 'MacBook Pro M1';
+let salePrice = 2399.99;
+let taxRate = 0.15;
+let calculatedTax = salePrice * taxRate;
+let total = salePrice + calculatedTax;
+
+console.log(computerModel);
+console.log(salePrice + " Sale Price");
+console.log(calculatedTax + " Tax Rate");
+console.log(total + " Final Sale");
